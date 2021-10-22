46ac12b2ce105b934c3fc778d985c8056adc9a35
Rui Oliveira <ruioliveira3@ua.pt>
2021-10-22 10:39:56 +0100

    home

--- example-app/resources/views/home.blade.php
+++ example-app/resources/views/home.blade.php
@@ -5,8 +5,38 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto Aula Web</title>
+    <link rel="stylesheet" href="topnav.css">
 </head>
 <body>
-    
+    <nav class="topnav">
+        <div>
+            
+        </div>
+        <ul class="nav-list" id="navi-list">
+            <li class="list-item">
+                <a href="#">Home</a>
+            </li>
+            <li class="list-item">
+                <a href="#">Conference</a>
+            </li>
+            <li class="list-item">
+                <a href="##">Account</a>
+            </li>
+        </ul>
+        <div class="menu" id="toggleButton">
+            <div class="menu-line"></div>
+            <div class="menu-line"></div>
+            <div class="menu-line"></div>
+        </div>
+    </nav>        
 </body>
+<script>
+    const toggleButton = document.getElementById('toggleButton');
+    const naviList = document.getElementById('navi-list');
+
+    toggleButton.addEventListener('click', () => {
+        navilist.classList.toggle('active');
+    })
+</script>
+
 </html>
