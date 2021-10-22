117ee6808ad0f1df2f57928f73cb83fa51a66c8d
Rui Oliveira <ruioliveira3@ua.pt>
2021-10-22 09:59:51 +0100

    Home

--- example-app/resources/views/conf.blade.php
+++ example-app/resources/views/conf.blade.php
@@ -5,9 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Conferencia</title>
-
 </head>
-<body>
- <p> <?= $conf; ?></p>   
+<div class="topnav">
+    <a class="active" href="#home">Home</a>
+    <a href="#conferenceInfo">Conference</a>
+    <a href="#accountInfo">Account</a>
+</div>    
+<body>    
 </body>
 </html>
