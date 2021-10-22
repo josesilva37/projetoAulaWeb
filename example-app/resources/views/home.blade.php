c043e7a8e50a53bfba054ed7fd2d296a497d9f51
josesilva37 <josesilva.jpas@gmail.com>
2021-10-22 10:32:10 +0000

    home

--- example-app/resources/views/home.blade.php
+++ example-app/resources/views/home.blade.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="../css/topnav.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto Aula Web</title>
 </head>
 <body>
-    
+    <div class="topnav">
+        <a class="active" href="#home">Home</a>
+        <a href="#conferenceInfo">Conference</a>
+        <a href="#accountInfo">Account</a>
+    </div>
+     
 </body>
 </html>
