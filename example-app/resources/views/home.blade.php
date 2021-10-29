7881a40aa5ae3c17de6510ac8013e60affd16733
kalebe <kaleberibeiro@ua.pt>
2021-10-29 11:37:26 +0100

    teste

--- example-app/resources/views/home.blade.php
+++ example-app/resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto Aula Web</title>
-    <link rel="stylesheet" href="../css/topbar.css">
+    <link rel="stylesheet" href="app.css">
 </head>
 <body>
     <div class="topnav">
