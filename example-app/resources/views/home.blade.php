276f40d407e4483ca56b6cfc67f77cc268524bdd
kalebe <kaleberibeiro@ua.pt>
2021-10-29 11:00:23 +0100

    teste

--- example-app/resources/views/home.blade.php
+++ example-app/resources/views/home.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="../resources/css/menu.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto Aula Web</title>
     <link rel="stylesheet" href="topnav.css">
+    <link rel="stylesheet" href="menu.css">
 </head>
 <body>
     <!-- <nav class="topnav">
