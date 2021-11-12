6ba7bc788068438508e48359de0890121bf494c9
kalebe <kaleberibeiro@ua.pt>
2021-11-12 10:56:45 +0000

    Update ControllerSponsor.php

--- example-app/app/Http/Controllers/ControllerSponsor.php
+++ example-app/app/Http/Controllers/ControllerSponsor.php
@@ -33,7 +33,7 @@ class ControllerSponsor extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store()
     {
         //
     }
@@ -78,7 +78,7 @@ class ControllerSponsor extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update($id)
     {
         return view('/');
     }
