56a7326969ff1bfb73c8229052bde5c9add11511
Rui Oliveira <ruioliveira3@ua.pt>
2021-11-12 10:31:44 +0000

    controller

--- example-app/app/Http/Controllers/Controller.php
+++ example-app/app/Http/Controllers/Controller.php
@@ -6,8 +6,83 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Models\Conference;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+   
+    /**
+    * Display a listing of the resource.
+    *
+    * @return Response
+    */
+    public function index()
+    {
+        //
+    }
+
+    /**
+    * Show the form for creating a new resource.
+    *
+    * @return Response
+    */
+    public function create()
+    {
+        //
+    }
+
+    /**
+    * Store a newly created resource in storage.
+    *
+    * @return Response
+    */
+    public function store()
+    {
+        //
+    }
+
+    /**
+    * Display the specified resource.
+    *
+    * @param  int  $id
+    * @return Response
+    */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+    * Show the form for editing the specified resource.
+    *
+    * @param  int  $id
+    * @return Response
+    */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+    * Update the specified resource in storage.
+    *
+    * @param  int  $id
+    * @return Response
+    */
+    public function update($id)
+    {
+        //
+    }
+    
+    /**
+    * Remove the specified resource from storage.
+    *
+    * @param  int  $id
+    * @return Response
+    */
+    public function destroy($id)
+    {
+        //
+    }
 }
