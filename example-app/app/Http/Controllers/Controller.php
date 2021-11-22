a973345a04674f0f118cc666fac0cdb3ddab9700
Rui Oliveira <ruioliveira3@ua.pt>
2021-11-22 12:29:34 +0000

    controller

--- example-app/app/Http/Controllers/Controller.php
+++ example-app/app/Http/Controllers/Controller.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use App\Models\Conference;
+use Illuminate\Http\Request;
 
 class Controller extends BaseController
 {
@@ -15,74 +16,89 @@ class Controller extends BaseController
     /**
     * Display a listing of the resource.
     *
-    * @return Response
+    * @return \Illuminate\Http\Response
     */
     public function index()
     {
         //
+        return view('conference', ['conf' => Conference::all()]);
     }
 
     /**
     * Show the form for creating a new resource.
     *
-    * @return Response
+    * @return \Illuminate\Http\Response
     */
     public function create()
     {
-        //
+        return redirect('/');
     }
 
     /**
-    * Store a newly created resource in storage.
-    *
-    * @return Response
-    */
-    public function store()
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
     {
-        //
+        $newConf = new Conference();
+        $newConf->name = $request->input('name');
+        $newConf->category = $request->input('category');
+        $newConf->save();
+
+        return redirect('/conference');
     }
 
     /**
     * Display the specified resource.
     *
     * @param  int  $id
-    * @return Response
+    * @return \Illuminate\Http\Response
     */
     public function show($id)
     {
-        //
+        return view('conferenceInfo', ['confI' => Conference::findOrFail($id)]);
     }
 
     /**
     * Show the form for editing the specified resource.
     *
     * @param  int  $id
-    * @return Response
+    * @return \Illuminate\Http\Response
     */
     public function edit($id)
     {
-        //
+        return view('/');
     }
 
     /**
-    * Update the specified resource in storage.
-    *
-    * @param  int  $id
-    * @return Response
-    */
-    public function update($id)
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function updateConf(Request $request, $id)
     {
-        //
+        $ConfEdit = Conference::find($id);
+        $ConfEdit->name = $request->input('name');
+        $ConfEdit->category = $request->input('category');
+        $ConfEdit->save();
+   
+        return redirect('/conference')->with('success', 'Conference updated');
     }
     
     /**
-    * Remove the specified resource from storage.
-    *
-    * @param  int  $id
-    * @return Response
-    */
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function destroy($id)
     {
-        //
+        Conference::destroy($id);
+
+        return redirect('/conference')->with('success', 'Conference deleted');
     }
 }
